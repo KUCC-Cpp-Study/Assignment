a3d342410d310e1dea9dc26244a6b26162a91356
InfDev00 <119485240+InfDev00@users.noreply.github.com>
2023-10-04 21:27:59 +0900

    Add day1

---
+++ day1/RHC/Ch1/1-4/1-4-func.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "1-4.h"
+using namespace part1_4;
+
+void BestComImpl::SimpleFunc() {
+	std::cout << "BestCom" << std::endl;
+}
+void ProgComImpl::SimpleFunc() {
+	std::cout << "ProgCom" << std::endl;
+}
---
+++ day1/RHC/Ch1/1-4/1-4main.cpp
@@ -0,0 +1,8 @@
+#include "1-4.h"
+
+namespace part1_4 {
+	void problem() {
+		BestComImpl::SimpleFunc();
+		ProgComImpl::SimpleFunc();
+	}
+}
